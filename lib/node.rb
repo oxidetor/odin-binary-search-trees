14935bb216aa0335c7db9851daeb834abec06885
oxidetor <prateek.girdhar@outlook.com>
2022-07-20 22:44:37 -0700

    Add method to insert values to binary search tree

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,7 @@
 class Node
   include Comparable
 
-  attr_reader :data, :left, :right
+  attr_accessor :data, :left, :right
 
   def initialize(data = nil, left = nil, right = nil)
     @data = data
--- lib/tree.rb
+++ lib/tree.rb
@@ -21,6 +21,24 @@ class Tree
     Node.new(array[middle], left, right)
   end
 
+  def insert(value, root = @root)
+    if value < root.data
+      return root.left = Node.new(value) if root.left.nil?
+
+      insert(value, root.left)
+    else
+      return root.right = Node.new(value) if root.right.nil?
+
+      insert(value, root.right)
+    end
+  end
+
+  def delete(value)
+    # TODO: When matched node is part of leaf
+    # TODO: When matched node has only one child node
+    # TODO: When matched node has two children
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -31,4 +49,7 @@ end
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new(array)
 puts tree.root.data
+tree.pretty_print
+tree.insert(3)
+tree.insert(4)
 tree.pretty_print
