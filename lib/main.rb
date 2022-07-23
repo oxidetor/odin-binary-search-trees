15e4cc7ee96e395400d17e630d704d1677e0cda4
oxidetor <prateek.girdhar@outlook.com>
2022-07-23 14:31:20 -0700

    Add simple driver script

---
+++ lib/main.rb
@@ -0,0 +1,24 @@
+require_relative './tree'
+
+tree = Tree.new((Array.new(15) { rand(1..100) }))
+tree.pretty_print
+puts "Balanced: #{tree.balanced?}"
+
+puts "Level Order: #{tree.level_order}"
+puts "Pre Order: #{tree.preorder}"
+puts "In Order: #{tree.inorder}"
+puts "Post Order: #{tree.postorder}"
+tree.insert(103)
+tree.insert(126)
+tree.insert(155)
+puts 'Inserted 3 values over 100...'
+tree.pretty_print
+puts "Balanced: #{tree.balanced?}"
+puts 'Rebalancing...'
+tree.rebalance
+tree.pretty_print
+puts "Balanced: #{tree.balanced?}"
+puts "Level Order: #{tree.level_order}"
+puts "Pre Order: #{tree.preorder}"
+puts "In Order: #{tree.inorder}"
+puts "Post Order: #{tree.postorder}"
